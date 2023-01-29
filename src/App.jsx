91dec47b77ac15543e5d73d3793e655227dc1983
Nate J.M <nate@natejonah.com>
2023-01-29 19:42:20 +0000

    Create Logos component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
 import Hero from './components/Hero'
+import Logos from './components/Logos'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Hero />
+      <Logos />
     </div>
   )
 }
---
+++ src/components/Logos.jsx
@@ -0,0 +1,12 @@
+export default function Logos() {
+    return (
+        <section className="logos">
+            <img src="./src/assets/logos/walmart.svg" />
+            <img src="./src/assets/logos/jpmorgan.svg" />
+            <img src="./src/assets/logos/visa.svg" />
+            <img src="./src/assets/logos/tinder.svg" />
+            <img src="./src/assets/logos/samsung.svg" />
+            <img src="./src/assets/logos/verizon.svg" />
+        </section>
+    )
+}
