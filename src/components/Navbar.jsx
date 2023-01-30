6d9d3caddab0254a40e4161b38b1cd7e46c8a700
Nate J.M <nate@natejonah.com>
2023-01-30 23:08:50 +0000

    Start styling navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
 export default function Navbar() {
     return (
-        <nav>
-            <h3 className="name">Name Surname</h3>
+        <nav className="bg-black">
+            <h3 className="text-white font-serif font-normal">Name Surname</h3>
 
             <ul className="menu">
-                <li><a href="#">Articles</a></li>
-                <li><a href="#">Chats</a></li>
-                <li><a href="#">Awards</a></li>
-                <li><a href="#">About</a></li>
+                <li><a className="text-grey" href="#">Articles</a></li>
+                <li><a className="text-grey" href="#">Chats</a></li>
+                <li><a className="text-grey" href="#">Awards</a></li>
+                <li><a className="text-grey" href="#">About</a></li>
             </ul>
 
-            <a className="contact-button" href="#">Get in touch</a>
+            <a className="bg-yellow no-underline text-black" href="#">Get in touch</a>
         </nav>
     )
 }
