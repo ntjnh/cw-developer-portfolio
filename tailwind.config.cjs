bb2f577b70bb3a3add3fa54353e1fac1e6f94b5c
Nate J.M <nate@natejonah.com>
2023-01-30 22:45:27 +0000

    Add tailwind and postcss configs

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.jsx"],
+  theme: {
+    colors: {
+      'black': '#0a0a0b',
+      'yellow': '#fbe850',
+      'gray': '#2e3038',
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      serif: ['Prata', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
