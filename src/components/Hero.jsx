d4573497fcfc028bbb790e4a34b8599a7e1b34b7
Nate J.M <nate@natejonah.com>
2023-01-30 23:07:03 +0000

    Start styling hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 export default function Hero() {
     return (
-        <section>
-            <h1>Helping companies build better, scalable software.</h1>
+        <section className="bg-black">
+            <h1 className="font-serif font-normal text-white">Helping companies build better, scalable software.</h1>
             <p>Award-winning web developer and author, with over 15+ years of working experience with Fortunee 500 companies like Apple, Google, Facebook, and more.</p>
         </section>
     )
