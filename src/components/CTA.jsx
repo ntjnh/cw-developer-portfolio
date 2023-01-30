324407fba8eef5af7dae6a61bf55d5c6f512e6cd
Nate J.M <nate@natejonah.com>
2023-01-30 23:08:05 +0000

    Start styling CTA component

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,9 @@
 export default function CTA() {
     return (
-        <section>
-            <h2>Interested in working with me?</h2>
+        <section className="bg-black">
+            <h2 className="font-serif font-normal text-white">Interested in working with me?</h2>
             <p>I'm active on all social media platforms listed below, but you can also send me an email and I will get back to you within 24-48 hours.</p>
-            <a href="#">Get in touch</a>
+            <a className="bg-yellow no-underline text-black" href="#">Get in touch</a>
         </section>
     )
 }
