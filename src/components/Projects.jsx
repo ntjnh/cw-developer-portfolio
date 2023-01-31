edb4ec5af0319da982a4dd4fce6a831c644e04ba
Nate J.M <nate@natejonah.com>
2023-01-31 21:17:37 +0000

    Start styling projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,17 +2,19 @@ import Project from './Project'
 
 export default function Projects(props) {
     return (
-        <section className="projects bg-black">
-            <Project
-                img="spense.png"
-                title="Spense.com"
-                description="Rethinking the way writers get paid, an open-source platform designed to help writers focus more on writing, and less on when and how they'll get paid. Project in collaboration with Codewell.cc"
-            />
-            <Project
-                img="yelpcamp.png"
-                title="YelpCamp.com"
-                description="Allowing backpack travellers to perfectly plan their trip through an open-source platform similar to TripAdvisor. With over 1m MAU, YelpCamp has been the crowd's favourite in 2021."
-            />
+        <section className="projects bg-black py-9">
+            <div className="container flex justify-between">
+                <Project
+                    img="spense.png"
+                    title="Spense.com"
+                    description="Rethinking the way writers get paid, an open-source platform designed to help writers focus more on writing, and less on when and how they'll get paid. Project in collaboration with Codewell.cc"
+                />
+                <Project
+                    img="yelpcamp.png"
+                    title="YelpCamp.com"
+                    description="Allowing backpack travellers to perfectly plan their trip through an open-source platform similar to TripAdvisor. With over 1m MAU, YelpCamp has been the crowd's favourite in 2021."
+                />
+            </div>
         </section>
     )
 }
