bc68cfa31b8e2175bc19ceea58284497dbe7a7e2
Nate J.M <nate@natejonah.com>
2023-01-31 21:43:58 +0000

    Amend Footer component styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,27 @@
 export default function Footer() {
     return (
-        <footer className="bg-black">
-            <h3 className="font-serif font-normal text-white">Name Surname</h3>
+        <footer className="bg-darkgrey py-9">
+            <div className="container flex align-center justify-between">
+                <h3 className="font-serif font-normal text-white">Name Surname</h3>
 
-            <ul>
-                <li>
-                    <a href="#">
-                        <img src="./src/assets/social-icons/twitter.svg" />
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <img src="./src/assets/social-icons/linkedin.svg" />
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <img src="./src/assets/social-icons/github.svg" />
-                    </a>
-                </li>
-            </ul>
+                <ul>
+                    <li className="inline-block px-2">
+                        <a href="#">
+                            <img src="./src/assets/social-icons/twitter.svg" />
+                        </a>
+                    </li>
+                    <li className="inline-block px-2">
+                        <a href="#">
+                            <img src="./src/assets/social-icons/linkedin.svg" />
+                        </a>
+                    </li>
+                    <li className="inline-block px-2">
+                        <a href="#">
+                            <img src="./src/assets/social-icons/github.svg" />
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </footer>
     )
 }
