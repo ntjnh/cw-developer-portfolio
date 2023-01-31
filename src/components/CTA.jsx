f04a43c1cbb1019c301eae43f7b278d6aaf2c353
Nate J.M <nate@natejonah.com>
2023-01-31 21:03:11 +0000

    Style CTA button

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -3,7 +3,7 @@ export default function CTA() {
         <section className="bg-black">
             <h2 className="font-serif font-normal text-white">Interested in working with me?</h2>
             <p>I'm active on all social media platforms listed below, but you can also send me an email and I will get back to you within 24-48 hours.</p>
-            <a className="bg-yellow no-underline text-black" href="#">Get in touch</a>
+            <a className="bg-yellow font-serif inline-block no-underline px-8 py-4 text-black" href="#">Get in touch</a>
         </section>
     )
 }
