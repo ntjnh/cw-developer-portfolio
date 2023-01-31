16b32835e797a9bdd09c20d02cddc258314e4220
Nate J.M <nate@natejonah.com>
2023-01-31 21:44:04 +0000

    Start styling Project component

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,9 @@
 export default function Project(props) {
     return (
         <div className="project">
-            <img src={`./src/assets/${props.img}`} alt="" />
-            <h2 className="font-serif font-normal text-white">{props.title} &#8594;</h2>
-            <p>{props.description}</p>
+            <img className="mb-6" src={`./src/assets/${props.img}`} alt={props.title} />
+            <h2 className="font-serif font-normal leading-snug mb-4 text-4xl text-white">{props.title} &#8594;</h2>
+            <p className="max-w-[530px] text-lg">{props.description}</p>
         </div>
     )
 }
