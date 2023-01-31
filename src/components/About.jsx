b1e6d7f6ba956cfa74089a67c047db39052872f3
Nate J.M <nate@natejonah.com>
2023-01-31 21:43:33 +0000

    Start styling About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,20 @@
 export default function About() {
     return (
-        <section className="bg-black">
-            <h2 className="font-serif text-white font-normal">A co-founder at one of the world's largest communities.</h2>
-            <p>The combined experience I have working at the top Fortune 500 companies has allowed me to develop a skillset that helps me in not just development, but in every aspect of any business.</p>
-            <p>I'm proud to announce that I am now working at one of the world's largest communities teaching young minds about how to sell yourself as a developer and open them to a whole new world of opportunities.</p>
-            <p>As a developer, you have evreything available to you and all that's holding you back is yourself.</p>
-            <p>A quote I live by perfectly illustrates what I mean.</p>
-            <p>"How big would you dream, if you knew you wouldn't fail? You've already gone through the hardest parts being a developer, it's time to elevate your skills and become a leader in something you're passionate about.</p>
-            <p>I'm happy to chat over coffee some time about how I can help your company.</p>
+        <section className="bg-darkgrey py-9">
+            <div className="align-center container flex justify-between">
+                <div className="max-w-[530px] w-1/2">
+                    <h2 className="font-normal font-serif flex-wrap leading-snug mb-8 text-4xl text-white">A co-founder at one of the world's largest communities.</h2>
+                    <p className="mb-6 text-lg">The combined experience I have working at the top Fortune 500 companies has allowed me to develop a skillset that helps me in not just development, but in every aspect of any business.</p>
+                    <p className="mb-6 text-lg">I'm proud to announce that I am now working at one of the world's largest communities teaching young minds about how to sell yourself as a developer and open them to a whole new world of opportunities.</p>
+                    <p className="mb-6 text-lg">As a developer, you have everything available to you and all that's holding you back is yourself.</p>
+                </div>
+
+                <div className="max-w-[530px]">
+                    <p className="mb-6 text-lg">A quote I live by perfectly illustrates what I mean.</p>
+                    <p className="mb-6 text-lg">"How big would you dream, if you knew you wouldn't fail? You've already gone through the hardest parts being a developer, it's time to elevate your skills and become a leader in something you're passionate about.</p>
+                    <p className="text-lg">I'm happy to chat over coffee some time about how I can help your company.</p>
+                </div>
+            </div>
         </section>
     )
 }
