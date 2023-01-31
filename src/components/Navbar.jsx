f095505c1972a454052a0b999281e460a2e02107
Nate J.M <nate@natejonah.com>
2023-01-31 21:03:00 +0000

    Style Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,18 @@
 export default function Navbar() {
     return (
         <nav className="bg-black">
-            <h3 className="text-white font-serif font-normal">Name Surname</h3>
+            <div className="container flex items-center justify-between py-9">
+                <h3 className="text-white font-serif font-normal">Name Surname</h3>
 
-            <ul className="menu">
-                <li><a className="text-grey" href="#">Articles</a></li>
-                <li><a className="text-grey" href="#">Chats</a></li>
-                <li><a className="text-grey" href="#">Awards</a></li>
-                <li><a className="text-grey" href="#">About</a></li>
-            </ul>
+                <ul className="menu">
+                    <li className="inline-block text-lg px-4"><a className="text-grey" href="#">Articles</a></li>
+                    <li className="inline-block text-lg px-4"><a className="text-grey" href="#">Chats</a></li>
+                    <li className="inline-block text-lg px-4"><a className="text-grey" href="#">Awards</a></li>
+                    <li className="inline-block text-lg px-4"><a className="text-grey" href="#">About</a></li>
+                </ul>
 
-            <a className="bg-yellow no-underline text-black" href="#">Get in touch</a>
+                <a className="bg-yellow font-serif inline-block no-underline px-8 py-4 text-black" href="#">Get in touch</a>
+            </div>
         </nav>
     )
 }
