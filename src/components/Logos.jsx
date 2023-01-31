ad5947687348108fd433953a4cedad39e2d60b58
Nate J.M <nate@natejonah.com>
2023-01-31 21:14:16 +0000

    Start styling logo component

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -1,12 +1,14 @@
 export default function Logos() {
     return (
-        <section className="bg-black">
-            <img src="./src/assets/logos/walmart.svg" />
-            <img src="./src/assets/logos/jpmorgan.svg" />
-            <img src="./src/assets/logos/visa.svg" />
-            <img src="./src/assets/logos/tinder.svg" />
-            <img src="./src/assets/logos/samsung.svg" />
-            <img src="./src/assets/logos/verizon.svg" />
+        <section className="bg-black py-9">
+            <div className="container align-center flex justify-between">
+                <img className="inline-block" src="./src/assets/logos/walmart.svg" />
+                <img className="inline-block" src="./src/assets/logos/jpmorgan.svg" />
+                <img className="inline-block" src="./src/assets/logos/visa.svg" />
+                <img className="inline-block" src="./src/assets/logos/tinder.svg" />
+                <img className="inline-block" src="./src/assets/logos/samsung.svg" />
+                <img className="inline-block" src="./src/assets/logos/verizon.svg" />
+            </div>
         </section>
     )
 }
