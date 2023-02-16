b5f31b4990a1335b7a7d9ea87c881d97bca39b38
Nate J.M <nate@natejonah.com>
2023-02-16 21:39:56 +0000

    Reduce project title margin

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,7 +2,7 @@ export default function Project(props) {
     return (
         <div className="project">
             <img className="mb-6" src={`./src/assets/${props.img}`} alt={props.title} />
-            <h2 className="font-serif font-normal leading-snug mb-4 text-4xl text-white">{props.title} &#8594;</h2>
+            <h2 className="font-serif font-normal leading-snug mb-2 text-4xl text-white">{props.title} &#8594;</h2>
             <p className="max-w-[530px] text-lg">{props.description}</p>
         </div>
     )
