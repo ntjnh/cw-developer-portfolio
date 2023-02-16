ba98458abcf69a991d7bdc8df122aaaf610d515e
Nate J.M <nate@natejonah.com>
2023-02-16 21:40:11 +0000

    Tweak Hero component spacing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,8 @@
 export default function Hero() {
     return (
-        <section className="bg-black py-9">
-            <div className="container">
-                <h1 className="font-serif font-normal leading-snug max-w-2xl text-5xl text-white">Helping companies build better, scalable software.</h1>
+        <section className="bg-black py-14">
+            <div className="container md:max-w-[1140px]">
+                <h1 className="font-serif font-normal leading-snug max-w-2xl text-[54px] text-white">Helping companies build better, scalable software.</h1>
                 <p className="max-w-2xl text-lg">Award-winning web developer and author, with over 15+ years of working experience with Fortunee 500 companies like Apple, Google, Facebook, and more.</p>
             </div>
         </section>
