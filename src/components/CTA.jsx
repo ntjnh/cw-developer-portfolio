c4ca8951c045d7915caed8ced03f8a86855b55c5
Nate J.M <nate@natejonah.com>
2023-02-16 21:42:43 +0000

    Tweak CTA component spacing

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,10 +1,12 @@
 export default function CTA() {
     return (
-        <section className="bg-black py-9">
-            <div className="container">
-                <h2 className="font-serif font-normal leading-snug mb-4 text-4xl text-white">Interested in working with me?</h2>
-                <p className="mb-4 text-lg">I'm active on all social media platforms listed below, but you can also send me an email and I will get back to you within 24-48 hours.</p>
-                <a className="bg-yellow font-serif inline-block no-underline px-8 py-4 text-black" href="#">Get in touch</a>
+        <section className="bg-black py-20">
+            <div className="container md:max-w-[1140px]">
+                <div className="max-w-[550px] w-1/2">
+                    <h2 className="font-serif font-normal leading-snug mb-0 text-4xl text-white">Interested in working with me?</h2>
+                    <p className="mb-4 text-lg">I'm active on all social media platforms listed below, but you can also send me an email and I will get back to you within 24-48 hours.</p>
+                    <a className="bg-yellow font-serif inline-block leading-none no-underline px-9 py-5 text-black text-lg" href="#">Get in touch</a>
+                </div>
             </div>
         </section>
     )
