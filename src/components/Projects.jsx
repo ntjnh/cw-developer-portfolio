1a8fc77668dcd9e9223996cd5559887818d165fb
Nate J.M <nate@natejonah.com>
2023-02-16 21:43:37 +0000

    Fix spacing in Projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,8 +2,8 @@ import Project from './Project'
 
 export default function Projects(props) {
     return (
-        <section className="projects bg-black py-9">
-            <div className="container flex justify-between">
+        <section className="projects bg-black py-20">
+            <div className="container flex justify-between md:max-w-[1140px]">
                 <Project
                     img="spense.png"
                     title="Spense.com"
@@ -12,7 +12,7 @@ export default function Projects(props) {
                 <Project
                     img="yelpcamp.png"
                     title="YelpCamp.com"
-                    description="Allowing backpack travellers to perfectly plan their trip through an open-source platform similar to TripAdvisor. With over 1m MAU, YelpCamp has been the crowd's favourite in 2021."
+                    description="Allowing backpack travelers to perfectly plan their trip through an open-source platform similar to TripAdvisor. With over 1m MAU, YelpCamp has been the crowd's favourite in 2021."
                 />
             </div>
         </section>
