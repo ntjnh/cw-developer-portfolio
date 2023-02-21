83a449db27467504ac8861fb48625fd6a3673561
Nate J.M <nate@natejonah.com>
2023-02-21 21:48:27 +0000

    Start Logos component mobile tweaks

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -1,11 +1,11 @@
 export default function Logos() {
     return (
-        <section className="bg-black py-9">
-            <div className="container align-center flex justify-between md:max-w-[1140px]">
-                <img className="inline-block" src="./src/assets/logos/walmart.svg" />
-                <img className="inline-block" src="./src/assets/logos/jpmorgan.svg" />
-                <img className="inline-block" src="./src/assets/logos/visa.svg" />
-                <img className="inline-block" src="./src/assets/logos/tinder.svg" />
+        <section className="bg-black px-5 md:px-0 py-5 md:py-9">
+            <div className="container align-center flex flex-wrap justify-around md:justify-between md:max-w-[1140px]">
+                <img className="inline-block py-2.5 md:py-0" src="./src/assets/logos/walmart.svg" />
+                <img className="inline-block py-2.5 md:py-0" src="./src/assets/logos/jpmorgan.svg" />
+                <img className="inline-block py-2.5 md:py-0" src="./src/assets/logos/visa.svg" />
+                <img className="inline-block py-2.5 md:py-0" src="./src/assets/logos/tinder.svg" />
                 <img className="inline-block" src="./src/assets/logos/samsung.svg" />
                 <img className="inline-block" src="./src/assets/logos/verizon.svg" />
             </div>
