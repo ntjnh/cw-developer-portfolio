b221cdca6cfe0da95c8c5da2ea51f05bb581ff1b
Nate J.M <nate@natejonah.com>
2023-02-21 21:49:19 +0000

    Start Navbar component mobile tweaks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,17 @@
 export default function Navbar() {
     return (
         <nav className="bg-black">
-            <div className="container flex items-center justify-between md:max-w-[1140px] py-9">
+            <div className="container flex items-center justify-between md:max-w-[1140px] px-5 md:px-0 py-9">
                 <h3 className="text-white font-serif font-normal">Name Surname</h3>
 
-                <ul className="menu">
+                <ul className="menu hidden md:block">
                     <li className="inline-block text-lg px-4"><a className="text-grey" href="#">Articles</a></li>
                     <li className="inline-block text-lg px-4"><a className="text-grey" href="#">Chats</a></li>
                     <li className="inline-block text-lg px-4"><a className="text-grey" href="#">Awards</a></li>
                     <li className="inline-block text-lg px-4"><a className="text-grey" href="#">About</a></li>
                 </ul>
 
-                <a className="bg-yellow font-serif inline-block leading-none no-underline px-9 py-5 text-black text-lg" href="#">Get in touch</a>
+                <a className="bg-yellow font-serif hidden md:inline-block leading-none no-underline px-9 py-5 text-black text-lg" href="#">Get in touch</a>
             </div>
         </nav>
     )
