573b47fa026ca622cc17168c92e80363fdfac70b
Nate J.M <nate@natejonah.com>
2023-02-21 21:47:49 +0000

    Start About component mobile tweaks

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,14 @@
 export default function About() {
     return (
-        <section className="bg-darkgrey py-14">
-            <div className="align-center container flex justify-between md:max-w-[1140px]">
-                <div className="max-w-[530px] w-1/2">
-                    <h2 className="font-normal font-serif flex-wrap leading-snug mb-4 text-4xl text-white">A co-founder at one of the world's largest communities.</h2>
+        <section className="bg-darkgrey px-5 md:px-0 py-14">
+            <div className="align-center container flex flex-wrap justify-between md:max-w-[1140px]">
+                <div className="md:max-w-[530px] md:w-1/2">
+                    <h2 className="font-normal font-serif flex-wrap leading-snug mb-4 text-3xl md:text-4xl text-white">A co-founder at one of the world's largest communities.</h2>
                     <p className="mb-6 text-lg">The combined experience I have working at the top Fortune 500 companies has allowed me to develop a skillset that helps me in not just development, but in every aspect of any business.</p>
                     <p className="mb-0 text-lg">I'm proud to announce that I am now working at one of the world's largest communities teaching young minds about how to sell yourself as a developer and open them to a whole new world of opportunities.</p>
                 </div>
 
-                <div className="max-w-[530px] w-1/2">
+                <div className="md:max-w-[530px] md:w-1/2">
                     <p className="mb-6 text-lg">As a developer, you have everything available to you and all that's holding you back is yourself.</p>
                     <p className="mb-6 text-lg">A quote I live by perfectly illustrates what I mean.</p>
                     <p className="mb-6 text-lg">"How big would you dream, if you knew you wouldn't fail? You've already gone through the hardest parts being a developer, it's time to elevate your skills and become a leader in something you're passionate about.</p>
