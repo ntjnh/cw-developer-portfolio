fe01d0bee4701c26137fe7f7031c9c930fc07230
Nate J.M <nate@natejonah.com>
2023-02-21 21:49:14 +0000

    Start CTA component mobile tweaks

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,9 @@
 export default function CTA() {
     return (
-        <section className="bg-black py-20">
+        <section className="bg-black px-5 md:px-0 py-12 md:py-20">
             <div className="container md:max-w-[1140px]">
-                <div className="max-w-[550px] w-1/2">
-                    <h2 className="font-serif font-normal leading-snug mb-0 text-4xl text-white">Interested in working with me?</h2>
+                <div className="md:max-w-[550px] md:w-1/2">
+                    <h2 className="font-serif font-normal leading-snug mb-3 text-3xl md:text-4xl text-white">Interested in working with me?</h2>
                     <p className="mb-4 text-lg">I'm active on all social media platforms listed below, but you can also send me an email and I will get back to you within 24-48 hours.</p>
                     <a className="bg-yellow font-serif inline-block leading-none no-underline px-9 py-5 text-black text-lg" href="#">Get in touch</a>
                 </div>
