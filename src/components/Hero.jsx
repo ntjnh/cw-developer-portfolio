6f1443d90ad1f0425245e3f0035f314053073ba0
Nate J.M <nate@natejonah.com>
2023-02-21 21:48:16 +0000

    Start Hero component mobile tweaks

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,8 @@
 export default function Hero() {
     return (
-        <section className="bg-black py-14">
+        <section className="bg-black px-5 md:px-0 pt-3 pb-7 md:py-14">
             <div className="container md:max-w-[1140px]">
-                <h1 className="font-serif font-normal leading-snug max-w-2xl text-[54px] text-white">Helping companies build better, scalable software.</h1>
+                <h1 className="font-serif font-normal leading-snug max-w-2xl mb-3 md:mb-0 text-3xl md:text-[54px] text-white">Helping companies build better, scalable software.</h1>
                 <p className="max-w-2xl text-lg">Award-winning web developer and author, with over 15+ years of working experience with Fortunee 500 companies like Apple, Google, Facebook, and more.</p>
             </div>
         </section>
