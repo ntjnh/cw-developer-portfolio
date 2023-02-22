c90c461947aafc28b292b0553c75c082e7917247
Nate J.M <nate@natejonah.com>
2023-02-22 08:50:50 +0000

    Reduce mobile font size in Project component

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,7 +3,7 @@ export default function Project(props) {
         <div className="project mb-10 md:mb-0">
             <img className="mb-6" src={`./src/assets/${props.img}`} alt={props.title} />
             <h2 className="font-serif font-normal leading-snug mb-2 text-3xl md:text-4xl text-white">{props.title} &#8594;</h2>
-            <p className="max-w-[530px] text-lg">{props.description}</p>
+            <p className="max-w-[530px] text-base md:text-lg">{props.description}</p>
         </div>
     )
 }
