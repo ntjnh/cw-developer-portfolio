5c6407157f7eff3ff0d60a54b7bfb6fab6188288
Nate J.M <nate@natejonah.com>
2023-02-22 08:50:12 +0000

    Tweak font size, button padding in CTA component

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -4,8 +4,8 @@ export default function CTA() {
             <div className="container md:max-w-[1140px]">
                 <div className="md:max-w-[550px] md:w-1/2">
                     <h2 className="font-serif font-normal leading-snug mb-3 text-3xl md:text-4xl text-white">Interested in working with me?</h2>
-                    <p className="mb-4 text-lg">I'm active on all social media platforms listed below, but you can also send me an email and I will get back to you within 24-48 hours.</p>
-                    <a className="bg-yellow font-serif inline-block leading-none no-underline px-9 py-5 text-black text-lg" href="#">Get in touch</a>
+                    <p className="mb-4 text-base md:text-lg">I'm active on all social media platforms listed below, but you can also send me an email and I will get back to you within 24-48 hours.</p>
+                    <a className="bg-yellow font-serif inline-block leading-none no-underline px-9 py-4 md:py-5 text-black text-lg" href="#">Get in touch</a>
                 </div>
             </div>
         </section>
