1a66840f326db1bfe143bc093e692fc99b9b2c02
Nate J.M <nate@natejonah.com>
2023-02-22 08:49:32 +0000

    Reduce mobile font sizes in About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,15 +4,15 @@ export default function About() {
             <div className="align-center container flex flex-wrap justify-between md:max-w-[1140px]">
                 <div className="md:max-w-[530px] md:w-1/2">
                     <h2 className="font-normal font-serif flex-wrap leading-snug mb-4 text-3xl md:text-4xl text-white">A co-founder at one of the world's largest communities.</h2>
-                    <p className="mb-6 text-lg">The combined experience I have working at the top Fortune 500 companies has allowed me to develop a skillset that helps me in not just development, but in every aspect of any business.</p>
-                    <p className="mb-0 text-lg">I'm proud to announce that I am now working at one of the world's largest communities teaching young minds about how to sell yourself as a developer and open them to a whole new world of opportunities.</p>
+                    <p className="mb-6 text-base md:text-lg">The combined experience I have working at the top Fortune 500 companies has allowed me to develop a skillset that helps me in not just development, but in every aspect of any business.</p>
+                    <p className="mb-0 text-base md:text-lg">I'm proud to announce that I am now working at one of the world's largest communities teaching young minds about how to sell yourself as a developer and open them to a whole new world of opportunities.</p>
                 </div>
 
                 <div className="md:max-w-[530px] md:w-1/2">
-                    <p className="mb-6 text-lg">As a developer, you have everything available to you and all that's holding you back is yourself.</p>
-                    <p className="mb-6 text-lg">A quote I live by perfectly illustrates what I mean.</p>
-                    <p className="mb-6 text-lg">"How big would you dream, if you knew you wouldn't fail? You've already gone through the hardest parts being a developer, it's time to elevate your skills and become a leader in something you're passionate about.</p>
-                    <p className="text-lg">I'm happy to chat over coffee some time about how I can help your company.</p>
+                    <p className="mb-6 text-base md:text-lg">As a developer, you have everything available to you and all that's holding you back is yourself.</p>
+                    <p className="mb-6 text-base md:text-lg">A quote I live by perfectly illustrates what I mean.</p>
+                    <p className="mb-6 text-base md:text-lg">"How big would you dream, if you knew you wouldn't fail? You've already gone through the hardest parts being a developer, it's time to elevate your skills and become a leader in something you're passionate about.</p>
+                    <p className="text-base md:text-lg">I'm happy to chat over coffee some time about how I can help your company.</p>
                 </div>
             </div>
         </section>
