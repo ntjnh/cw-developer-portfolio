c0953fedb34e2f08c17e27f22871d9c477b94165
Nate J.M <nate@natejonah.com>
2023-02-25 13:08:08 +0000

    Tweak Hero component spacing on tablet screen size

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,8 @@
 export default function Hero() {
     return (
-        <section className="bg-black px-5 md:px-0 pt-3 pb-7 md:py-14">
+        <section className="bg-black px-5 md:px-0 pt-3 pb-7 lg:py-14">
             <div className="container md:max-w-screen-sm lg:max-w-screen-md xl:max-w-[1140px]">
-                <h1 className="font-serif font-normal leading-snug md:leading-normal lg:leading-snug max-w-2xl mb-3 lg:mb-0 text-3xl md:text-4xl lg:text-[54px] text-white">Helping companies build better, scalable software.</h1>
+                <h1 className="font-serif font-normal leading-snug md:leading-normal lg:leading-snug max-w-2xl mb-3 lg:mb-0 text-3xl md:text-5xl lg:text-[54px] text-white">Helping companies build better, scalable software.</h1>
                 <p className="max-w-2xl text-lg">Award-winning web developer and author, with over 15+ years of working experience with Fortunee 500 companies like Apple, Google, Facebook, and more.</p>
             </div>
         </section>
