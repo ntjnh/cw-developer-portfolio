d25035b4e01f1deed8a782316b6e6bced8adaaea
Nate J.M <nate@natejonah.com>
2023-02-25 13:08:31 +0000

    Tweak tablet spacing for Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,22 +9,22 @@ export default function Navbar() {
     }
 
     return (
-        <nav className="bg-black">
-            <div className="container flex flex-wrap items-center justify-between md:max-w-[1140px] px-5 md:px-0 pt-9 pb-3 md:py-9">
+        <nav className="bg-black px-5 md:px-0">
+            <div className="container flex flex-wrap items-center justify-between md:max-w-screen-sm lg:max-w-screen-md xl:max-w-[1140px] md:px-0 pt-9 pb-3 md:py-9">
                 <h3 className="text-white font-serif font-normal">Name Surname</h3>
 
-                <button className="inline-block md:hidden" onClick={toggleMenu}>
+                <button className="inline-block lg:hidden" onClick={toggleMenu}>
                     <img src={menuIcon} alt="menu toggle" />
                 </button>
 
-                <ul className="menu hidden md:block mt-4 md:mt-0 text-center w-full">
-                    <li className="inline-block text-lg px-2 md:px-4"><a className="text-grey" href="#">Articles</a></li>
-                    <li className="inline-block text-lg px-2 md:px-4"><a className="text-grey" href="#">Chats</a></li>
-                    <li className="inline-block text-lg px-2 md:px-4"><a className="text-grey" href="#">Awards</a></li>
-                    <li className="inline-block text-lg px-2 md:px-4"><a className="text-grey" href="#">About</a></li>
+                <ul className="menu hidden lg:block mt-4 lg:mt-0 text-center w-full lg:w-auto">
+                    <li className="inline-block text-lg px-2 lg:px-4"><a className="text-grey" href="#">Articles</a></li>
+                    <li className="inline-block text-lg px-2 lg:px-4"><a className="text-grey" href="#">Chats</a></li>
+                    <li className="inline-block text-lg px-2 lg:px-4"><a className="text-grey" href="#">Awards</a></li>
+                    <li className="inline-block text-lg px-2 lg:px-4"><a className="text-grey" href="#">About</a></li>
                 </ul>
 
-                <a className="bg-yellow font-serif hidden md:inline-block leading-none no-underline px-9 py-5 text-black text-lg" href="#">Get in touch</a>
+                <a className="bg-yellow font-serif hidden lg:inline-block leading-none no-underline px-4 lg:px-9 py-3 lg:py-5 text-black text-lg" href="#">Get in touch</a>
             </div>
         </nav>
     )
