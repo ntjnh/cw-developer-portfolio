4b7f8e0abad54296e5d90a12a62b6a55d15d6d2a
Nate J.M <nate@natejonah.com>
2023-02-25 00:57:09 +0000

    Adjust CTA component for tablet screen size

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,11 +1,11 @@
 export default function CTA() {
     return (
-        <section className="bg-black px-5 md:px-0 py-12 md:py-20">
-            <div className="container md:max-w-[1140px]">
-                <div className="md:max-w-[550px] md:w-1/2">
+        <section className="bg-black px-5 md:px-0 py-12 lg:py-20">
+            <div className="container md:max-w-screen-sm lg:max-w-screen-md xl:max-w-[1140px]">
+                <div className="md:max-w-[532px] lg:max-w-[550px] lg:w-1/2">
                     <h2 className="font-serif font-normal leading-snug mb-3 text-3xl md:text-4xl text-white">Interested in working with me?</h2>
                     <p className="mb-4 text-base md:text-lg">I'm active on all social media platforms listed below, but you can also send me an email and I will get back to you within 24-48 hours.</p>
-                    <a className="bg-yellow font-serif inline-block leading-none no-underline px-9 py-4 md:py-5 text-black text-lg" href="#">Get in touch</a>
+                    <a className="bg-yellow font-serif inline-block leading-none no-underline px-9 py-4 lg:py-5 text-black text-lg" href="#">Get in touch</a>
                 </div>
             </div>
         </section>
