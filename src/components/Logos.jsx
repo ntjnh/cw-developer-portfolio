f58200f5fc27fff30a7c3152fba4065f33d7ed68
Nate J.M <nate@natejonah.com>
2023-02-25 13:26:40 +0000

    Add logo components to parent

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -9,12 +9,12 @@ export default function Logos() {
     return (
         <section className="bg-black px-5 md:px-0 py-7 md:py-9">
             <div className="container align-center flex flex-wrap justify-around md:justify-center lg:justify-between md:max-w-screen-sm lg:max-w-screen-md xl:max-w-[1140px]">
-                <img className="inline-block px-8 lg:px-0 mb-4 md:mb-6 lg:my-0" src="./src/assets/logos/walmart.svg" />
-                <img className="inline-block px-8 lg:px-0 mb-4 md:mb-6 lg:my-0" src="./src/assets/logos/jpmorgan.svg" />
-                <img className="inline-block px-8 lg:px-0 mb-4 md:mb-6 lg:my-0" src="./src/assets/logos/visa.svg" />
+                <Walmart />
+                <JPMorgan />
+                <Visa />
                 <Tinder />
-                <img className="inline-block px-8 lg:px-0" src="./src/assets/logos/samsung.svg" />
-                <img className="inline-block px-8 lg:px-0" src="./src/assets/logos/verizon.svg" />
+                <Samsung />
+                <Verizon />
             </div>
         </section>
     )
