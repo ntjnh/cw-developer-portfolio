a3d27c670d62abde25e03830a4e26ad8844b0c8c
Nate J.M <nate@natejonah.com>
2023-02-25 13:07:47 +0000

    Swap Tinder SVG file for component with svg code
    Also adjusts Logos component spacing

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -1,13 +1,15 @@
+import Tinder from "../assets/logos/Tinder"
+
 export default function Logos() {
     return (
         <section className="bg-black px-5 md:px-0 py-7 md:py-9">
-            <div className="container align-center flex flex-wrap justify-around md:justify-between md:max-w-[1140px]">
-                <img className="inline-block py-2.5 md:py-0" src="./src/assets/logos/walmart.svg" />
-                <img className="inline-block py-2.5 md:py-0" src="./src/assets/logos/jpmorgan.svg" />
-                <img className="inline-block py-2.5 md:py-0" src="./src/assets/logos/visa.svg" />
-                <img className="inline-block py-2.5 md:py-0" src="./src/assets/logos/tinder.svg" />
-                <img className="inline-block" src="./src/assets/logos/samsung.svg" />
-                <img className="inline-block" src="./src/assets/logos/verizon.svg" />
+            <div className="container align-center flex flex-wrap justify-around md:justify-center lg:justify-between md:max-w-screen-sm lg:max-w-screen-md xl:max-w-[1140px]">
+                <img className="inline-block px-8 lg:px-0 mb-4 md:mb-6 lg:my-0" src="./src/assets/logos/walmart.svg" />
+                <img className="inline-block px-8 lg:px-0 mb-4 md:mb-6 lg:my-0" src="./src/assets/logos/jpmorgan.svg" />
+                <img className="inline-block px-8 lg:px-0 mb-4 md:mb-6 lg:my-0" src="./src/assets/logos/visa.svg" />
+                <Tinder />
+                <img className="inline-block px-8 lg:px-0" src="./src/assets/logos/samsung.svg" />
+                <img className="inline-block px-8 lg:px-0" src="./src/assets/logos/verizon.svg" />
             </div>
         </section>
     )
