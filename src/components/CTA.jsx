75ec20c3c06eba8eef16cab9d6738450616625cb
Nate J.M <nate@natejonah.com>
2023-02-25 15:58:39 +0000

    Reduce CTA button spacing

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -5,7 +5,7 @@ export default function CTA() {
                 <div className="md:max-w-[532px] lg:max-w-[550px] lg:w-1/2">
                     <h2 className="font-serif font-normal leading-snug mb-3 text-3xl md:text-4xl text-white">Interested in working with me?</h2>
                     <p className="mb-4 text-base md:text-lg">I'm active on all social media platforms listed below, but you can also send me an email and I will get back to you within 24-48 hours.</p>
-                    <a className="bg-yellow font-serif inline-block leading-none no-underline px-4 lg:px-9 py-3 lg:py-5 text-black text-sm lg:text-lg" href="#">Get in touch</a>
+                    <a className="bg-yellow font-serif inline-block leading-none no-underline px-4 lg:px-7 py-3 text-black text-sm lg:text-lg" href="#">Get in touch</a>
                 </div>
             </div>
         </section>
